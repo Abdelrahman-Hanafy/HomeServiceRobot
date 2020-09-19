959a1291986728bac98165307a665c80f30b9e4a
Abdelrahman-Hanafy <Abdelrahman.m.moustafa@gmail.com>
2020-09-19 11:39:55 +0200

    writing main file to launch the final project

---
+++ src/scripts/main.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+xterm -e "roscore" &
+sleep 5
+xterm -e "roslaunch my_robot world.launch" &
+sleep 5
+xterm -e "roslaunch my_robot amcl.launch" &
+sleep 5
+xterm -e "roslaunch my_robot navigation.launch"&
+sleep 5 
+xterm -hold -e "roslaunch add_markers marker.launch"
+sleep 5
+xterm -hold -e "roslaunch pick_object pick.launch " 
